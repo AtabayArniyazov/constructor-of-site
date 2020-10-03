27d46dcb9a300a4535f45bbd4f1bb2fd31c57d85
Atabay <Atabaj.Arniyazov@p97.com>
2020-10-04 01:00:40 +0300

    [constructor-of-site] --- added classes and improved the model; added new css function to utils.js; removed the templates.js;

--- src/model.js
+++ src/model.js
@@ -1,15 +1,47 @@
 import image from './assets/image.png';
+import { TitleBlock, TextBlock, ColumnsBlock, ImageBlock } from './classes/blocks';
+
+const text = `My github <a href="https://github.com/AtabayArniyazov">repository</a>`;
 
 export const model = [
-    {
-        type: 'title',
-        value: 'Constructor of sites on pure JS!',
-        options: {
-            tag: 'h2',
-            styles: `background: linear-gradient(to right, #ff0099, #493240); color: #fff; text-align: center; padding: 1.5rem;`
+    new TitleBlock('Constructor of sites on pure JS!', {
+        tag: 'h2',
+        styles: {
+            background: 'linear-gradient(to right, #ff0099, #493240)',
+            color: '#fff',
+            'text-align': 'center',
+            padding: '1.5rem'
         }
-    },
-    {type: 'text', value: 'Here we go some text'},
-    {type: 'columns', value: ['1111', '2222', '3333']},
-    {type: 'image', value: image}
+    }),
+    new ImageBlock(image, {
+        styles: {
+            padding: '2rem 0',
+            display: 'flex',
+            'justify-content': 'center'
+        },
+        imageStyles: {
+            width: '500px',
+            height: 'auto'
+        },
+        alt: 'This is an image'
+    }),
+    new TextBlock(text, {
+        styles: {
+            background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+            padding: '1rem',
+            'font-weight': 'bold'
+        }
+    }),
+    new ColumnsBlock([
+        'Pure JS application',
+        'Тo libraries required',
+        'JS is simple, interesting. Learn to create any UI with your own hands'
+    ], {
+        styles: {
+            background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+            padding: '2rem',
+            color: '#fff',
+            'font-weight': 'bold'
+        }
+    })
 ];
